664f124123a3758dad443142d12d017e56443be4
Josch <j.o.scholz@me.com>
2020-12-07 17:54:14 +0100

    feat(tailwind): add tailwind css

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './tailwind.css';
 import Index from './components/app';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Index />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Index />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
