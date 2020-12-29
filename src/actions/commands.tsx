d22b8147f35e8fbe657939df19534ad9aa3adc36
Josch <j.o.scholz@me.com>
2020-12-29 01:35:39 +0100

    chore(machine): add actions and useMachine - wip

---
+++ src/actions/commands.tsx
@@ -0,0 +1,14 @@
+export enum Commands {
+	READ = 'READ',
+	WRITE = 'WRITE',
+	LOAD = 'LOAD',
+	STORE = 'STORE',
+	ADD = 'ADD',
+	SUB = 'SUB',
+	MULT = 'MULT',
+	DIV = 'DIV',
+	JUMP = 'JUMP',
+	JZERO = 'JZERO',
+	JGTZ = 'JGTZ',
+	HALT = 'HALT',
+}
---
+++ src/hooks/useMachine.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { Commands } from '../actions/commands';
+
+export function useMachine(
+	programCounter: number,
+	commandLine: string[],
+	input: number,
+	register: number[]
+) {
+	let pc = programCounter;
+	const command = commandLine[1];
+	const argument = Number(commandLine[2]);
+	let output;
+	let acc = register[0];
+
+	switch (command) {
+		case Commands.READ:
+			{
+				register[argument] = input;
+			}
+			break;
+		case Commands.WRITE:
+			{
+				output = register[argument];
+			}
+			break;
+		case Commands.LOAD:
+			{
+				acc = register[argument];
+			}
+			break;
+		case Commands.STORE:
+			{
+				register[argument] = acc;
+			}
+			break;
+		case Commands.ADD:
+			{
+				acc += register[argument];
+			}
+			break;
+		case Commands.SUB:
+			{
+				acc -= register[argument];
+			}
+			break;
+		case Commands.MULT:
+			{
+				acc *= register[argument];
+			}
+			break;
+		case Commands.DIV:
+			{
+				acc /= register[argument];
+			}
+			break;
+		case Commands.JUMP:
+			{
+				pc = argument;
+			}
+			break;
+		case Commands.JZERO:
+			{
+				pc = acc === 0 ? argument : pc;
+			}
+			break;
+		case Commands.JGTZ:
+			{
+				pc = acc > 0 ? argument : pc;
+			}
+			break;
+		case Commands.HALT:
+			{
+			}
+			break;
+		default:
+			{
+			}
+			break;
+	}
+
+	register[0] = acc;
+
+	return {
+		programCounter: pc,
+		register: register,
+		output: output,
+	};
+}
