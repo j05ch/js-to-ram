cf3a3ee112aeeb8f64a3dd72720b6d2a4e151231
Josch <j.o.scholz@me.com>
2021-04-25 13:34:10 +0200

    feat(ram): add acc-styles to registers

--- src/components/random-access-machine/register/index.tsx
+++ src/components/random-access-machine/register/index.tsx
@@ -13,13 +13,22 @@ const Register: React.FC<Props> = ({ register, index, mark }) => {
 
 	return (
 		<>
-			<div className={colorMark}>
-				<div>
+			{index === 0 ? (
+				<div
+					className={`p-1 pr-4 border-4 border-yellow-500 flex justify-between bg-yellow-500 ${colorMark}`}
+				>
 					<div>
 						{index}:{register}
 					</div>
+					<div className="font-bold">ACC</div>
 				</div>
-			</div>
+			) : (
+				<div className={colorMark}>
+					<div className="p-1 pl-1.5">
+						{index}:{register}
+					</div>
+				</div>
+			)}
 		</>
 	);
 };
